c9dc07ac82428e40beb6741b6ce3b69f060b4289
DESKTOP-T291JB0\AI <ahmedelshawadfy2015@gmail.com>
2021-06-20 14:03:47 -0700

    Last version of I2C of Tiva C Master sending and arduino Slave

--- Tiva C/I2C_Master/I2C.c
+++ Tiva C/I2C_Master/I2C.c
@@ -8,6 +8,67 @@
 
 #include "I2C.h"
 
+// I2C3 intialization and GPIO alternate function configuration
+void I2C3_Init ( void )
+{
+SYSCTL->RCGCGPIO  |= 0x00000008 ; // Enable the clock for port D
+SYSCTL->RCGCI2C   |= 0x00000008 ; // Enable the clock for I2C 3
+//ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C3);
+
+// Configure Port D pins 0 and 1 as I2C 3
+GPIOD->AFSEL |= 0x00000003 ;
+GPIOD->PCTL |= 0x00000033 ;
+GPIOD->DEN |= 0x03; // Assert DEN for port D
+GPIOD->ODR |= 0x00000002 ; // SDA (PD1 ) pin as open darin
+I2C3->MCR  = 0x0010 ; // Enable I2C 3 master function
+/* Configure I2C 3 clock frequency
+(1 + TIME_PERIOD ) = SYS_CLK /(2*
+( SCL_LP + SCL_HP ) * I2C_CLK_Freq )
+TIME_PERIOD = 16 ,000 ,000/(2(6+4) *100000) - 1 = 7 */
+I2C3->MTPR  = 0x07 ;
+}
+
+/* wait untill I2C Master module is busy */
+/*  and if not busy and no error return 0 */
+static int I2C_wait_till_done(void)
+{
+    while(I2C3->MCS & 1);   /* wait until I2C master is not busy */
+    return I2C3->MCS & 0xE; /* return I2C error code, 0 if no error*/
+}
+
+// Receive one byte of data from I2C slave device
+char I2C3_Write_Multiple(int slave_address, char slave_memory_address, int bytes_count, char* data)
+{   
+    char error;
+    if (bytes_count <= 0)
+        return -1;                  /* no write was performed */
+    /* send slave address and starting address */
+    I2C3->MSA = slave_address << 1;
+    I2C3->MDR = slave_memory_address;
+    I2C3->MCS = 3;                  /* S-(saddr+w)-ACK-maddr-ACK */
+
+    error = I2C_wait_till_done();   /* wait until write is complete */
+    if (error) return error;
+
+    /* send data one byte at a time */
+    while (bytes_count > 1)
+    {
+        I2C3->MDR = *data++;             /* write the next byte */
+        I2C3->MCS = 1;                   /* -data-ACK- */
+        error = I2C_wait_till_done();
+        if (error) return error;
+        bytes_count--;
+    }
+    
+    /* send last byte and a STOP */
+    I2C3->MDR = *data++;                 /* write the last byte */
+    I2C3->MCS = 5;                       /* -data-ACK-P */
+    error = I2C_wait_till_done();
+    while(I2C3->MCS & 0x40);             /* wait until bus is not busy */
+    if (error) return error;
+    return 0;       /* no error */
+}
+
 /**********************************************************************/
 //initialize I2C module 0
 //Slightly modified version of TI's example code
@@ -16,13 +77,20 @@ void InitI2C0(void)
     //enable I2C module 0
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C3);
 
+    //reset module
+    //ROM_SysCtlPeripheralReset(SYSCTL_PERIPH_I2C3);
+
     //enable GPIO peripheral that contains I2C 0
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
     // Configure the pin muxing for I2C0 functions on port B2 and B3.
     ROM_GPIOPinTypeI2C(GPIO_PORTD_BASE, GPIO_PIN_0);
     ROM_GPIOPinTypeI2C(GPIO_PORTD_BASE, GPIO_PIN_1);
-    
+    //GPIOD->ODR |= 0x00000002 ; // SDA (PD1 ) pin as open darin
+    //ROM_GPIOPinTypeGPIOOutputOD (GPIO_PORTD_BASE,GPIO_PD1_I2C3SDA | GPIO_PIN_1);
+    ROM_GPIOPinConfigure(GPIO_PD0_I2C3SCL);
+    ROM_GPIOPinConfigure(GPIO_PD1_I2C3SDA);
+
     // Select the I2C function for these pins.
     ROM_GPIOPinTypeI2CSCL(GPIO_PORTD_BASE, GPIO_PIN_0);
     
